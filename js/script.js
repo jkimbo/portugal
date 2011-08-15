cf58211ac0a612d05d9bea0440aef4b2325a32f2
Jonathan Kim <jkimbo@gmail.com>
2011-08-15 16:01:48 +0100

    Initial commit. Upload works and basic style. Need to connect to a database

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-/* Author:
+/* Author: J.Kim
 
 */
 
-
-
+$(document).ready(function() {
+ $('#file_upload').uploadify({
+    'uploader'  : 'uploadify/uploadify.swf',
+    'script'    : 'uploadify/uploadify.php',
+    'cancelImg' : 'uploadify/cancel.png',
+    'folder'    : 'uploads',
+    'auto'      : true,
+    'multi'     : true,
+    'buttonText': 'Upload Files',
+    'fileExt'   : '*.jpg;*.gif;*.png',
+    'fileDesc'  : 'Image Files',
+    'onInit'    : function() {
+        $('#upload').show();
+    },
+    'onAllComplete' : function(event, data) {
+        $('#thankyou #info').text(data.filesUploaded + ' files uploaded successfully!');
+        $('#upload').fadeOut(300, function() {
+            $('#thankyou').fadeIn(400, function() {
+                $('#thankyou').fadeOut(4000, function() {
+                    $('#upload').fadeIn(300);
+                });                   
+            });
+        });
+    }
+  });
+});
 
 
