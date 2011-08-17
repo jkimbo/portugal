858b659279f74bf6890be791bd82feb25e18f932
Jonathan Kim <jkimbo@gmail.com>
2011-08-18 00:33:00 +0100

    Made page ready for photo gallery (upload photos is now a hidden menu)

--- js/script.js
+++ js/script.js
@@ -3,30 +3,34 @@
 */
 
 $(document).ready(function() {
- $('#file_upload').uploadify({
-    'uploader'  : 'uploadify/uploadify.swf',
-    'script'    : 'uploadify/uploadify.php',
-    'cancelImg' : 'uploadify/cancel.png',
-    'folder'    : 'uploads',
-    'auto'      : true,
-    'multi'     : true,
-    'buttonText': 'Upload Files',
-    'fileExt'   : '*.jpg;*.gif;*.png',
-    'fileDesc'  : 'Image Files',
-    'onInit'    : function() {
-        $('#upload').show();
-    },
-    'onAllComplete' : function(event, data) {
-        $('#thankyou #info').text(data.filesUploaded + ' files uploaded successfully!');
-        $('#upload').fadeOut(300, function() {
-            $('#thankyou').fadeIn(400, function() {
-                $('#thankyou').fadeOut(4000, function() {
-                    $('#upload').fadeIn(300);
-                });                   
+     $('#file_upload').uploadify({
+        'uploader'  : 'uploadify/uploadify.swf',
+        'script'    : 'uploadify/uploadify.php',
+        'cancelImg' : 'uploadify/cancel.png',
+        'folder'    : 'uploads',
+        'auto'      : true,
+        'multi'     : true,
+        'buttonText': 'Upload Files',
+        'fileExt'   : '*.jpg;*.gif;*.png',
+        'fileDesc'  : 'Image Files',
+        'onAllComplete' : function(event, data) {
+            $('#thankyou #info').text(data.filesUploaded + ' files uploaded successfully!');
+            $('#upload').fadeOut(300, function() {
+                $('#thankyou').fadeIn(400, function() {
+                    $('#thankyou').fadeOut(3000, function() {
+                        $('#upload_prompt').fadeIn(400);
+                    });                   
+                });
             });
+        }
+      });
+
+    $('#upload_prompt a').click(function() {
+        $('#upload_prompt').fadeOut(300, function() {
+            $('#upload').fadeIn(400);
         });
-    }
-  });
+        return false;
+    });
 });
 
 
